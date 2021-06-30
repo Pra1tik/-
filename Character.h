e69c3f76025ae80c6f9bc60442e69e6491226ce4
Pratik <pratikhadkhale@gmail.com>
2021-06-30 20:08:24 +0545

    Base character class added

---
+++ Character.h
@@ -0,0 +1,22 @@
+#pragma once
+
+
+class Character
+{
+    public:
+        Character() {};
+
+        virtual void update() {};
+        virtual void render() {};
+
+        void setPosition(int x, int y) { mPosX = x; mPosY = y; }
+        void setVelocity(int x, int y) { mVelX = x; mVelY = y; }
+        int getPositionX() {return mPosX; }
+        int getPositionY() {return mPosY; }
+        int getVelocityX() {return mVelX; }
+        int getVelocityY() {return mVelY; }
+
+    private:
+        int mPosX, mPosY;
+        int mVelX, mVelY;
+};
