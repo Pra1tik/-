842ffcc6fc5088185d3738bf79a43803099f9ebf
Satkar <satkar147258369@gmail.com>
2021-07-14 10:32:10 +0545

    Collision can be detected but cant fall down

--- Player.cpp
+++ Player.cpp
@@ -8,10 +8,10 @@ Player::Player(SDL_Renderer* renderer)
 
     mTexture = new TextureWrapper(renderer);
     
-    mTexture->loadFromFile("graphics/index.png");
+    mTexture->loadFromFile("assets/player.png");
 
     //Set initial position
-    setPosition(100, 600);
+    setPosition(100, 300);
     setVelocity(10, imp);
     setAccleration(0, 0);
 
@@ -19,10 +19,42 @@ Player::Player(SDL_Renderer* renderer)
 
 }
 
-
-void Player::update()
+void Player::CollisionUpdate(vec offset,vec tile)
 {
-    //Update the positions
+    // std::cout<<tile.y<<std::endl;
+    // // if(mAcc.y == 1)
+    // // {
+    // //     mVel.y = (mVel.y - gravity);
+    // //     mPos.y -= (mVel.y * dt); 
+    // // }
+
+    // if(mPos.y > offset.y+300)  //this needs to be change after calculating collisions
+    if(offset.x==0)  //this needs to be change after calculating collisions
+    {
+        
+        mVel.y = imp;
+        mAcc.y = 0;
+        mPos.y += (offset.y-25);
+    }
+    // if (tile.y != 0)
+    // {
+    //     mPos.y= tile.y*50+290-110;
+    // }
+    if(offset.y ==0)  //this needs to be change after calculating collisions
+    {
+        mVel.y = imp;
+        mAcc.y = 0;
+        mPos.y = 600;
+    }
+    // mPos.y += offset.y*dt;
+    mPos.x += offset.x;
+}
+
+
+void Player::update(int tileNum)
+{
+
+    // Update the positions
     if(mAcc.y == 1)
     {
         mVel.y = (mVel.y - gravity);
@@ -35,11 +67,9 @@ void Player::update()
         mAcc.y = 0;
         mPos.y = 600;
     }
+    
 
     mPos.x += mAcc.x * mVel.x;
-
-
-
 }
 
 
--- game.cpp
+++ game.cpp
@@ -1,5 +1,7 @@
 #include "game.h"
 #include "Player.h"
+#include "Tilemap.h"
+#include "collider.h"
 
 bool Game::init(const char* title,int xpos, int ypos, int height, int width, bool flags)
 {
@@ -32,6 +34,10 @@ bool Game::init(const char* title,int xpos, int ypos, int height, int width, boo
         else{
             return false;
         }
+        back = new Background(pRenderer);
+        back->load();
+        pCollider= new collider();
+        tCollider= new collider();
         runGame= true;
         return true;
     }
@@ -41,13 +47,15 @@ bool Game::init(const char* title,int xpos, int ypos, int height, int width, boo
 void Game::render()
 {
     SDL_RenderClear(pRenderer);
+    back->render();
     player->render();
     SDL_RenderPresent(pRenderer);
 }
 
 void Game::update()
 {
-    player->update();
+   if(!(colX&&colY)) {player->update(tileBelow);}
+//    else if (colX&&colY) {player->CollisionUpdate(offsetValue);}
 }
 
 void Game::clean()
@@ -73,5 +81,63 @@ void Game::handleEvents(float deltaTime)
 
         }
     }
+    Collider(player,back);
 
+}
+
+void Game::Collider(Player* player,Background* back)
+{   
+    // vec collidedTile;
+    vec player1=player->getPosition();
+    SDL_Rect pCol{player1.x,player1.y,80,110};
+    pCollider->setRect(pCol);
+    
+    for (int row {0};row<12;row++)
+    {
+        for ( int column{0};column<16;column++)
+        {
+            SDL_Rect pTil{back->getPosXTiles(row,column),back->getPosYTiles(row,column),48,48};
+            tCollider->setRect(pTil);
+            vec tile={column,row};
+            // std::cout<<back->getPosYTiles(row,column)<<std::endl;
+            if (back->getTileNum(row,column)==1)
+            {
+            if(pCollider->checkCollision(*tCollider,0.0f))
+            {
+                
+                colY=true;
+                colX=true;
+                // std::cout << tCollider->GetPositionY()<<"   "<<row<<std::endl;
+                // collidedTile.y=row;
+                // collidedTile.x=column;
+                vec offset=pCollider->getOffset();
+                if (pCollider->GetPositionX()>tCollider->GetPositionX()){offset.x*=-1;}
+                if (pCollider->GetPositionX()<tCollider->GetPositionX() && pCollider->GetPositionX()+80>tCollider->GetPositionX()+48){offset.x=0;}
+                if (pCollider->GetPositionX()>tCollider->GetPositionX() && pCollider->GetPositionX()<tCollider->GetPositionX()+48){offset.x=0;}
+                // if (pCollider->GetPositionY()>tCollider->GetPositionY() && pCollider->GetPositionY()<tCollider->GetPositionY()+48){offset.y=0;}
+                if (pCollider->GetPositionY()>tCollider->GetPositionY()){offset.y*=-1;}
+                // if (pCollider->GetPositionY()<tCollider->GetPositionY()+48){offset.y=0;}
+                // std::cout << "collision detected in y-axis with tile "<<column<<"   "<<row<<"     "<<count++<<std::endl;
+                player->CollisionUpdate(offset,tile);
+                break;
+            }
+            else
+            {
+                tileBelow=back->getTileNum(row+1,column);
+                colY=false;
+                colX=false;
+            }
+            // if(player1.x+80>=TileX && TileX+48>=player1.x )
+            // {
+            //     colXPos=true;
+            //     collidedTileX=column;
+            //     std::cout << "collision detected in x-axis with tile "<<row<<"   "<<column<<"     "<<count++<<std::endl;
+            //     break;
+            // }
+            // else{
+            //     colXPos=false;
+            // }
+            }     
+        }
+    }
 }
