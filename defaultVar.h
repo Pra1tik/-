0495fc60009e9cd8938b1eaa15052abd6c402daf
Satkar <satkar147258369@gmail.com>
2021-07-18 19:07:33 +0545

    default variables header added

---
+++ defaultVar.h
@@ -0,0 +1,11 @@
+
+
+#define WindowWidth 1280
+#define WindowHeight 720
+
+
+#define TileWidth 50
+#define TileHeight 50
+
+#define TotalTilesRow WindowWidth/TileWidth
+#define TotalTilesColumn WindowHeight/TileHeight
