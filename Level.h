0e36cfe6a64c41c7a9743e5f7231c7b5d7d22f38
Pratik <pratikhadkhale@gmail.com>
2021-08-09 11:32:03 +0545

    Level class added

---
+++ Level.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "Texture.h"
+#include "Player.h"
+#include "math.h"
+#include "game.h"
+#include <cstdio>
+
+class Level
+{
+    public:
+        int mLevelNum;
+        int mLWidth, mLHeight;
+        int mRow, mCol;
+        int tileWidth, tileHeight;
+        int scale = 4;
+        SDL_Rect* clips;
+        int** mTileMatrix;
+        TextureWrapper* mTexture;
+        SDL_Renderer* mRenderer;
+        SDL_Rect camera;
+        //class Game* mGame = NULL;
+    public:
+        Level(const char filename[], SDL_Renderer* renderer, int levelNum);
+        ~Level();
+
+        void update(vec pPos, TextureWrapper* pTexture);
+        void render();
+       // void setGame(class Game* game) { mGame = game; }
+
+};
