16a0667490b8151973cc2b45977a1c8467e966a8
Pratik <pratikhadkhale@gmail.com>
2021-08-23 15:48:01 +0545

    Archer can shoot arrow

--- Enemy2.cpp
+++ Enemy2.cpp
@@ -11,37 +11,87 @@ Enemy2::Enemy2(SDL_Renderer* renderer, vec pos)
         std::cout << "Failed to load enemy.\n";
     }
 
+    arrowTexture = new TextureWrapper(renderer);
+    arrowTexture->loadFromFile("graphics/newBullet.png");
+
     dead = false;
     stopRendering = false;
 }
 
 
-int Enemy2::update(vec pPos, SDL_Rect Bullet) //2 for collsion with bullet
+int Enemy2::update(vec pPos) 
 {
     if(!dead)
     {
         SDL_Rect pRect = {pPos.x, pPos.y, playerWidth, playerHeight};
         SDL_Rect eRect = {ePos.x, ePos.y, eTexture->getWidth()/6, eTexture->getHeight()};
 
+        if(arrow1.arrowAlive)
+        {
+            arrow1.arrowtPos.x += arrow1.arrowVel.x;
+
+            if(abs(arrow1.arrowtPos.x - pPos.x) >= 500)
+            {
+                arrow1.arrowAlive = false;
+            }
+        }
+
+        
         if(!(pRect.x < (eRect.x + eRect.w) && (pRect.x + pRect.w) > eRect.x &&
             pRect.y < (eRect.y + eRect.h) && (pRect.y + pRect.h) > eRect.y))
         {
-            //Shoot
-            float dsquare = (pPos.x-ePos.x)*(pPos.x-ePos.x) + (pPos.y-ePos.y)*(pPos.y-ePos.y);
-            if(dsquare <= 1000)
+           //Shoot the player
+            int dx = abs(pPos.x-ePos.x);
+            int dy = abs(pPos.y - ePos.y);
+            if(!arrow1.arrowAlive && dx <= 3000 && dy <= playerHeight)
             {
-                return 1;
+                //std::cout << "SHoot" << std::endl;
+                arrow1.arrowAlive = true;
+                arrow1.arrowtPos = {ePos.x, ePos.y+eTexture->getHeight()/2};
+
+                arrow1.arrowVel.x = (pPos.x >= ePos.x ? 10 : -10);
             }
+                
         }
-        if (Bullet.x < (eRect.x + eRect.w) && (Bullet.x + Bullet.w)> eRect.x &&
-            Bullet.y < (eRect.y + eRect.h) && (Bullet.y + Bullet.h) > eRect.y)
+        else
         {
-            dead = true;
-            return 2;
+            return 3;
         }
+        
     }
 
     return 0;
+}
+
+bool Enemy2::bulletEnemyCollision(const SDL_Rect& Bullet)
+{
+    SDL_Rect eRect = {ePos.x, ePos.y, eTexture->getWidth()/6, eTexture->getHeight()};
+    
+   if(Bullet.x < (eRect.x + eRect.w) && (Bullet.x + Bullet.w)> eRect.x &&
+            Bullet.y < (eRect.y + eRect.h) && (Bullet.y + Bullet.h) > eRect.y)
+    {
+        dead = true;
+        return true;
+    }
+
+    return false;
+}
+
+bool Enemy2::arrowPlayerCollision(const SDL_Rect& playerRect)
+{
+    if(arrow1.arrowAlive)
+    {
+        SDL_Rect arrowRect = {arrow1.arrowtPos.x, arrow1.arrowtPos.y, arrowTexture->getWidth()/6, arrowTexture->getHeight()};
+    
+        if(arrowRect.x < (playerRect.x + playerRect.w) && (arrowRect.x + arrowRect.w)> playerRect.x &&
+                    arrowRect.y < (playerRect.y + playerRect.h) && (arrowRect.y + arrowRect.h) > playerRect.y)
+        {
+            arrow1.arrowAlive = false;
+            return true;
+        }
+    }
+
+    return false;
 }
 
 void Enemy2::render(SDL_Rect camera, vec pPos)
@@ -65,6 +115,11 @@ void Enemy2::render(SDL_Rect camera, vec pPos)
     if(!dead)
     {
         eTexture->render(src, dest, 0.0, flip);
+    }
+
+    if(arrow1.arrowAlive)
+    {
+        arrowTexture->render(arrow1.arrowtPos.x-camera.x, arrow1.arrowtPos.y-camera.y);
     }
 }
 
--- Enemy2.h
+++ Enemy2.h
@@ -8,16 +8,28 @@ class Enemy2
         Enemy2(SDL_Renderer* renderer, vec pos);
         ~Enemy2();
 
-        int update(vec pPos, SDL_Rect Bullet);
+        int update(vec pPos);
         void render(SDL_Rect camera, vec pPos);
-
+        bool bulletEnemyCollision(const SDL_Rect& Bullet);
+        bool arrowPlayerCollision(const SDL_Rect& player);
 
         vec ePos;
         TextureWrapper* eTexture;
         bool dead;
         int nextShot = 0;
+
+        struct arrow
+        {
+            vec arrowtPos;
+            vec arrowVel = {10, 2};
+            bool shootFlag = false;
+            bool arrowAlive = false;
+        };
+        arrow arrow1;
+        
     private:
-        bool stopRendering;  
+        bool stopRendering;
+        TextureWrapper* arrowTexture;  
 };
 
 
